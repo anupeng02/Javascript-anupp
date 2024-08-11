9f029e268e97bc9c16f7b166bcbc5e4e97d9569e
anupeng02 <anupsofteng@gmail.com>
2024-08-11 03:21:13 +0000

    changes the variables name

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,17 @@
+const account_Id = 45679;
+
+let account_Email = "anup@google.com";
+
+var account_Password = "12345";
+
+account_City = "Varanasi";
+
+// account_Id = 2;   // not allowed 
+
+account_Email = "ag@ag.com";
+
+
+console.log(account_Id);
+
+// console.table([account_Id, account_Email, account_City, account_Password])
+
