3a63a9c86f955e70c4158d53270de940cb6d82cb
anupeng02 <anupsofteng@gmail.com>
2024-08-15 12:47:15 +0000

    callApplyBind1.js file is commited

---
+++ callApplyBind/callApplyBind1.js
@@ -0,0 +1,22 @@
+let userDetails ={
+    name : "anup gupta",
+    age: 25,
+    designation : "react Developer",
+    
+}
+ function printDetails(){
+    console.log(this.name)
+    console.log(this.designation)
+}
+
+printDetails.call(userDetails)
+
+
+let userDetails2 ={
+    name : "Raja Ram",
+    age: 25,
+    designation : "IAS",
+    
+}
+
+printDetails.call(userDetails2);
