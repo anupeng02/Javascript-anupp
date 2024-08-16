ff53906b1dfcd6e26e3de1d9f4a62c9909e261c7
anupeng02 <anupsofteng@gmail.com>
2024-08-16 11:52:35 +0000

    comm filter

--- mapReduceFilter/filter.js
+++ mapReduceFilter/filter.js
@@ -1,1 +1,19 @@
 let arr = [7, 8, 3, 10, 2,  12, 15, 13, 11];
+
+// function isEven(x){
+//    return x%2==0;
+// }
+
+// function isOdd(x){
+//     return x%2==0;
+//  }
+
+//  let output = arr.filter(isEven);
+
+let even = arr.filter((x)=>x%2==0)
+
+ console.log(even)
+
+ let odd = arr.filter((x)=>x%2)
+
+ console.log(odd);
