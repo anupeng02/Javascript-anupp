ae22216779a95cc34e4a8e783b3b32119d791251
Mr. Anup Gupta <anupsofteng@gmail.com>
2024-08-20 12:15:29 +0000

    dataType

---
+++ 01-basics/02-dataType.js
@@ -0,0 +1,31 @@
+"use strict"  // treat all js code as a newer version
+
+// alert(3+3);  // we are using  node.js not browser
+
+console.log(3+3) // code readability should be high
+
+console.log("Anup gupta") //
+
+let name ="anup"
+
+let age = 18
+
+let loggedIn = true
+
+//number 2^53 
+
+// string => "" or ''
+
+//boolean => true/false
+
+// null => it is standalone value  and it represents emty value
+
+// undefined =>  it is anot assigned any value till now
+
+// symbol
+
+// object
+
+console.log(typeof null)  // object
+
+console.log(typeof undefined)   // undefined
